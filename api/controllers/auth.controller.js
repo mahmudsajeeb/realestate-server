c5ba88a119f51f633178c071bf04e02479877c08
Mahmud sajib <mahmudsajib082@gmail.com>
2023-10-14 13:13:18 +0600

    sign up create

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,5 +1,18 @@
-const express = require('express');
-const signup = async (req,res) =>{
+ 
+const   User  = require('../models/usersModel.js');
+const bcryptjs = require('bcryptjs')
+const signup = async  (req,res) =>{
+    const {username,email,password} = req.body;
+    const hashedPassword = bcryptjs.hashSync(password,10)
+    const newUser = new User ({username,email,password:hashedPassword})
+   
+
+  try{
+    await  newUser.save();
+    res.status(201).json("User Created Successfully")
+  }catch(err){
+    res.status(500).json(err.message)
+  }
   
 }
 
